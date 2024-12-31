720b3779eeeabad7650d15884c58c6ed5922d7ed
Leafmw <ankang4869@gmail.com>
2024-12-31 17:52:44 +0800

    路由学习

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+// app/dashboard/layout.js
+export default function DashboardLayout({
+    children,
+  }) {
+    return (
+      <section>
+        <nav>nav</nav>
+        {children}
+      </section>
+    )
+  }
+  
